c92a497f1bdb9336c2b559472ee3223228f5d492
SilverBread <98303464+SilverBread@users.noreply.github.com>
2022-02-21 00:39:04 +0900

    Create member_thank.php

---
+++ demo/member_thank.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>登録完了 | White BookCafe</title>
+    <link href="css/member_thank.css" rel="stylesheet">
+  </head>
+  <body>
+    <main>
+      <h2>会員登録完了</h2>
+      <p class="message">
+        White Book Cafeの定額会員サービスにご登録いただきまして誠にありがとうございます。<br>
+        落ち着いた空間で それぞれの時間をお楽しみくださいませ。<br>
+        今後ともどうぞ White Book Cafe をよろしくお願い申し上げます。
+      </p>
+      <div class="btn">
+        <a href="/portforio1-cafe/index.html">トップへ戻る</a>
+      </div>
+    </main>
+  </body>
+</html>
