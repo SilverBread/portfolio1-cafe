91c7e21e2a9147d44881b10eb4ab35748bba9b2c
SilverBread <98303464+SilverBread@users.noreply.github.com>
2022-02-20 23:27:04 +0900

    Create call_complete.php

---
+++ public/call_complete.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>送信完了 | White BookCafe</title>
+    <link href="css/call_complete.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <h2>送信完了</h2>
+    <p class="message">
+    お問い合わせありがとうございます。1営業日以内にご返信させていただきます。<br>
+    自動返信メールをお送りしておりますのでご確認ください。<br>
+    <br>
+    ※サンプルのためメールは送信されません
+    </p>
+    <p class="back"><a href="index.html">トップに戻る</a></p>
+  </body>
+</html>
